78607bf76ab2f9cd4ac4c0cc7ae8609a0752663c
Ainul.Habib <habib.ainul@fmr.com>
2023-04-20 12:04:52 +0100

    added cyress ui test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,19 @@
-describe('My First Test', () => {
+describe('E2E UI Test', () => {
 
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
+  before(() => {
+    cy.visit("http://localhost:32137", { timeout: 30000 });
+  });
+
+
+  it('should visit', () => {
+    cy.wait(1000);
+    cy.get('form').within(($form) => {
+      cy.get('input[name="email"]').type('eaxample@email.com')
+      cy.get('input[name="systolic"]').type('120')
+      cy.get('input[name="diastolic"]').type('98')
+      cy.root().submit()
+    })
+
+  });
 
 })

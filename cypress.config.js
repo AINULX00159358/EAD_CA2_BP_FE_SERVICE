4a8a9a1e7d0e1aa8d996ac1e2413a1ffc71844ab
ainulhabib <ainul.habib@paddypowerbetfair.com>
2023-04-21 21:57:17 +0100

    updated e2e

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
+       baseUrl = 'http://4.207.193.144:32137/',
        setUpNodeEvents(on, config){
        },
         supportFile: false,
